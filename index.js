75907efbe1deb9af0c1335cb764876c85f6fbdf0
beka_ab01 <bekaabera01@gmail.com>
2021-10-01 11:43:00 -0700

    setuping recycle bin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let inputEl = document.getElementById("input-el");
 let saveBtn = document.getElementById("save-btn");
 let ulEl = document.getElementById("ul-el");
 let ulEl2 = document.getElementById("prev-lists");
+let recycleBtn = document.getElementById("recycle-btn");
 let myList = [];
 
 saveBtn.addEventListener("click", function () {
@@ -28,11 +29,15 @@ function previousBtn() {
 }
 
 function clearLists() {
-        window.alert("Your Saved To-Do List will be deleted permanently!");
+        window.alert("Your Saved To-Do List will be deleted. How ever if you want to get it back go to  the 'Bin list'!");
         ulEl.innerHTML = "";
         ulEl2.innerHTML = "";
         localStorage.clear();
     }
-function recycleBtn() {
-    localStorage.setItem("recycle", recyledItems);
+function rec() {
+    alert("hello world this is Beka abera")
+
 }
+recycleBtn.addEventListener("click", function() {
+    alert("hello");
+})
